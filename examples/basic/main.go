096a1c7ff00b3d25e7bf3c9575cc611387abe1f0
Jason Blanchard <jas.blanchard@gmail.com>
2020-09-13 08:49:06 -0400

    Removed everything

--- examples/basic/main.go
+++ examples/basic/main.go
@@ -1,25 +1,25 @@
 package main
 
-import (
-	"github.com/jasonblanchard/natsby"
-	"github.com/nats-io/nats.go"
-)
+// import (
+// 	"github.com/jasonblanchard/natsby"
+// 	"github.com/nats-io/nats.go"
+// )
 
-func main() {
-	nc, err := nats.Connect(nats.DefaultURL)
-	engine, err := natsby.New(nc)
-	if err != nil {
-		panic(err)
-	}
+// func main() {
+// 	nc, err := nats.Connect(nats.DefaultURL)
+// 	engine, err := natsby.New(nc)
+// 	if err != nil {
+// 		panic(err)
+// 	}
 
-	logger := natsby.DefaultLogger()
-	engine.Use(natsby.WithLogger(logger))
+// 	logger := natsby.DefaultLogger()
+// 	engine.Use(natsby.WithLogger(logger))
 
-	engine.Subscribe("ping", natsby.WithByteReply(), func(c *natsby.Context) {
-		c.ByteReplyPayload = []byte("pong")
-	})
+// 	engine.Subscribe("ping", natsby.WithByteReply(), func(c *natsby.Context) {
+// 		c.ByteReplyPayload = []byte("pong")
+// 	})
 
-	engine.Run(func() {
-		logger.Info().Msg("Ready 🚀")
-	})
-}
+// 	engine.Run(func() {
+// 		logger.Info().Msg("Ready 🚀")
+// 	})
+// }
--- natsby_test.go
+++ natsby_test.go
@@ -1,101 +1,101 @@
 package natsby
 
-import (
-	"os"
-	"testing"
-	"time"
+// import (
+// 	"os"
+// 	"testing"
+// 	"time"
 
-	"github.com/nats-io/nats.go"
-	"github.com/stretchr/testify/assert"
-)
+// 	"github.com/nats-io/nats.go"
+// 	"github.com/stretchr/testify/assert"
+// )
 
-func TestNew(t *testing.T) {
-	nc, _ := nats.Connect(nats.DefaultURL)
-	engine, _ := New(nc)
+// func TestNew(t *testing.T) {
+// 	nc, _ := nats.Connect(nats.DefaultURL)
+// 	engine, _ := New(nc)
 
-	assert.Equal(t, os.Stdout, engine.OutWriter)
-	assert.Equal(t, os.Stderr, engine.ErrWriter)
-	assert.Equal(t, nc, engine.NatsConnection)
-}
+// 	assert.Equal(t, os.Stdout, engine.OutWriter)
+// 	assert.Equal(t, os.Stderr, engine.ErrWriter)
+// 	assert.Equal(t, nc, engine.NatsConnection)
+// }
 
-func TestNewOptions(t *testing.T) {
-	nc, _ := nats.Connect(nats.DefaultURL)
-	var c *nats.EncodedConn
-	configureEncodedConnection := func(e *Engine) error {
-		var err error
-		c, err = nats.NewEncodedConn(e.NatsConnection, nats.JSON_ENCODER)
-		if err != nil {
-			return err
-		}
-		e.NatsEncodedConnection = c
-		return nil
-	}
-	engine, _ := New(nc, configureEncodedConnection)
+// func TestNewOptions(t *testing.T) {
+// 	nc, _ := nats.Connect(nats.DefaultURL)
+// 	var c *nats.EncodedConn
+// 	configureEncodedConnection := func(e *Engine) error {
+// 		var err error
+// 		c, err = nats.NewEncodedConn(e.NatsConnection, nats.JSON_ENCODER)
+// 		if err != nil {
+// 			return err
+// 		}
+// 		e.NatsEncodedConnection = c
+// 		return nil
+// 	}
+// 	engine, _ := New(nc, configureEncodedConnection)
 
-	assert.Equal(t, os.Stdout, engine.OutWriter)
-	assert.Equal(t, os.Stderr, engine.ErrWriter)
-	assert.Equal(t, nc, engine.NatsConnection)
-	assert.Equal(t, c, engine.NatsEncodedConnection)
-}
+// 	assert.Equal(t, os.Stdout, engine.OutWriter)
+// 	assert.Equal(t, os.Stderr, engine.ErrWriter)
+// 	assert.Equal(t, nc, engine.NatsConnection)
+// 	assert.Equal(t, c, engine.NatsEncodedConnection)
+// }
 
-func TestUse(t *testing.T) {
-	e := Engine{}
-	handler := func(c *Context) {}
+// func TestUse(t *testing.T) {
+// 	e := Engine{}
+// 	handler := func(c *Context) {}
 
-	e.Use(handler)
+// 	e.Use(handler)
 
-	assert.Len(t, e.middleware, 1)
-}
+// 	assert.Len(t, e.middleware, 1)
+// }
 
-func TestSubscribe(t *testing.T) {
-	engine := Engine{}
-	handler := func(c *Context) {}
+// func TestSubscribe(t *testing.T) {
+// 	engine := Engine{}
+// 	handler := func(c *Context) {}
 
-	engine.Subscribe("test.subject", handler)
+// 	engine.Subscribe("test.subject", handler)
 
-	assert.Equal(t, 1, len(engine.subscribers))
-}
+// 	assert.Equal(t, 1, len(engine.subscribers))
+// }
 
-func TestRun(t *testing.T) {
-	nc, _ := nats.Connect(nats.DefaultURL)
-	engine, _ := New(nc)
-	handler := func(c *Context) {
-		c.ByteReplyPayload = []byte("pong")
-	}
-	engine.Subscribe("test.subject", WithByteReply(), handler)
-	didCallCallback := false
-	callback := func() {
-		didCallCallback = true
-	}
+// func TestRun(t *testing.T) {
+// 	nc, _ := nats.Connect(nats.DefaultURL)
+// 	engine, _ := New(nc)
+// 	handler := func(c *Context) {
+// 		c.ByteReplyPayload = []byte("pong")
+// 	}
+// 	engine.Subscribe("test.subject", WithByteReply(), handler)
+// 	didCallCallback := false
+// 	callback := func() {
+// 		didCallCallback = true
+// 	}
 
-	go engine.Run(callback)
+// 	go engine.Run(callback)
 
-	// Let the listeners regiser
-	time.Sleep(1 * time.Second)
+// 	// Let the listeners regiser
+// 	time.Sleep(1 * time.Second)
 
-	_, err := nc.Request("test.subject", []byte(""), 1*time.Second)
-	if err != nil {
-		panic(err)
-	}
+// 	_, err := nc.Request("test.subject", []byte(""), 1*time.Second)
+// 	if err != nil {
+// 		panic(err)
+// 	}
 
-	engine.Shutdown()
+// 	engine.Shutdown()
 
-	assert.True(t, didCallCallback)
-}
+// 	assert.True(t, didCallCallback)
+// }
 
-func TestRunQueue(t *testing.T) {
-	nc, _ := nats.Connect(nats.DefaultURL)
-	configureQueueGroup := func(e *Engine) error {
-		e.QueueGroup = "group"
-		return nil
-	}
-	engine, _ := New(nc, configureQueueGroup)
-	handler := func(c *Context) {}
-	engine.Subscribe("test.subject", handler)
+// func TestRunQueue(t *testing.T) {
+// 	nc, _ := nats.Connect(nats.DefaultURL)
+// 	configureQueueGroup := func(e *Engine) error {
+// 		e.QueueGroup = "group"
+// 		return nil
+// 	}
+// 	engine, _ := New(nc, configureQueueGroup)
+// 	handler := func(c *Context) {}
+// 	engine.Subscribe("test.subject", handler)
 
-	go engine.Run()
+// 	go engine.Run()
 
-	engine.Shutdown()
+// 	engine.Shutdown()
 
-	assert.Equal(t, true, true, "Engine started and shutdown")
-}
+// 	assert.Equal(t, true, true, "Engine started and shutdown")
+// }
